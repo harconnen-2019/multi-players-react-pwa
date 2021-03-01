e43ccad6b5cb4faef37c83d93bcfc0693eaa3687
Harconnen <harconnen.public@gmail.com>
2021-03-01 21:21:15 +0300

    Настройка проекта

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as config from './config'
 
 function App() {
+  console.log(config.VERSION)
+  console.log(process.env.NODE_ENV)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gray-800 text-white h-screen'>
+      <p>
+        Edit 1<code>src/App.tsx</code> and save to reload.
+      </p>
+      <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+        Learn React
+      </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/config.tsx
@@ -0,0 +1,12 @@
+import { version } from '../package.json'
+
+/**
+ * Версия приложения
+ * @constant
+ */
+export const VERSION = `App version : ${version} (alfa)`
+/**
+ * Инициализация приложения
+ * @constant
+ */
+export const URL_INIT = '/api/orange/func/player/init'
