f7e7803fd91788080eff05b83a2246b91158f5e1
Harconnen <harconnen.public@gmail.com>
2021-03-02 16:50:23 +0300

    Получение данных из init api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import * as config from './config'
+import { getCookie, createInit, getData } from './lib/init'
 
 function App() {
-  console.log(config.VERSION)
-  console.log(process.env.NODE_ENV)
+  const [init, setInit] = useState({})
+  const [initStatus, setInitStatus] = useState('init') // init, loading, loaded, error
+
+  useEffect(() => {
+    consolTitle()
+    setInitStatus('loading')
+    getData(
+      `${config.PREFIX + config.URL_INIT}?session=${getCookie('session')}`
+    )
+      .then((data) => {
+        const result: object = createInit(data)
+        setInit(result)
+      })
+      .catch((e) => {
+        setInitStatus('error')
+      })
+  }, [])
+
+  function consolTitle(): void {
+    console.group('Init player:')
+    console.info(config.VERSION)
+    config.DEBUG && console.log('env : ' + process.env.NODE_ENV)
+    config.DEBUG && console.log('session : %s', getCookie('session'))
+    console.groupEnd()
+  }
 
   return (
     <div className='bg-gray-800 text-white h-screen'>
--- src/config.tsx
+++ src/config.tsx
@@ -1,12 +1,28 @@
+/**
+ * Модуль с настройками плеера
+ * @module config
+ */
+
 import { version } from '../package.json'
 
 /**
  * Версия приложения
  * @constant
  */
-export const VERSION = `App version : ${version} (alfa)`
+export const VERSION: string = `App version : ${version} (alfa)`
+/**
+ * Debug
+ * @constant
+ */
+export const DEBUG: boolean =
+  process.env.NODE_ENV === 'development' ? true : false
+/**
+ * Префикс для локального запуска
+ * @constant
+ */
+export const PREFIX: string = 'https://default.my-radio.app'
 /**
  * Инициализация приложения
  * @constant
  */
-export const URL_INIT = '/api/orange/func/player/init'
+export const URL_INIT: string = '/api/orange/func/player/init'
