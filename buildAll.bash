442c1a516dae48d214a5281a88fe8aa566481fa0
Harconnen <harconnen.public@gmail.com>
2021-05-27 18:52:21 +0300

    setting

--- buildAll.bash
+++ buildAll.bash
@@ -8,6 +8,7 @@ mkdir server/ok
 mkdir server/vk
 mkdir server/vkm
 mkdir server/win
+mkdir server/android
 
 cp -r build/* server
 cp -r build/* server/cws
@@ -16,13 +17,15 @@ cp -r build/* server/ok
 cp -r build/* server/vk
 cp -r build/* server/vkm
 cp -r build/* server/win
+cp -r build/* server/android
 
 echo '{"app":"pwa"}' > server/settings.json
-echo '{"app":"cws"}' > server/cws/ssettings.json
+echo '{"app":"cws"}' > server/cws/settings.json
 echo '{"app":"fb"}'  > server/fb/settings.json
 echo '{"app":"ok"}'  > server/ok/settings.json
 echo '{"app":"vk"}'  > server/vk/settings.json
 echo '{"app":"vkm"}' > server/vkm/settings.json
 echo '{"app":"win"}' > server/win/settings.json
+echo '{"app":"android"}' > server/android/settings.json
 
 exit;
