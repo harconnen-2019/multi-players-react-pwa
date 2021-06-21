97709be0e3c042b9b7682e0e19fbe932dde59d38
Harconnen <harconnen.public@gmail.com>
2021-06-21 10:52:02 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -291,12 +291,12 @@ function App() {
       background_color: '#ffffff',
       icons: [
         {
-          src: favicon.replace('-x-', '192x192'),
+          src: window.location.href + favicon.replace('-x-', '192x192'),
           type: 'image/png',
           sizes: '192x192',
         },
         {
-          src: favicon.replace('-x-', '278x278'),
+          src: window.location.href + favicon.replace('-x-', '278x278'),
           type: 'image/png',
           sizes: '278x278',
         },
